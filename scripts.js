0960abd51f142e147fe9529cbf32b4ef053990ff
jastakpawel <innyjastak@gmail.com>
2017-05-30 12:58:14 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var a = prompt('Wpisz pierwszą zmienną');
+var b = prompt('Wpisz drugą zmienną');
+var wynikRownania = (a * a) + (2 * a * b) - (b * b);
+console.log('Equation with a: ' + a + ' and b: ' + b + ' equals: ' + wynikRownania);
+if(wynikRownania > 0)  {
+ 	console.log('Wynik równania jest liczbą dodatnią.')
+} else if(wynikRownania < 0) {
+	console.log('Wynik równania jest liczbą ujemną.') 
+} else if(wynikRownania == 0)  {
+ alert('Wynik równania jest równy zero')
+}
